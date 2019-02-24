4e81c079467c933a433529f30557f24a61b008b9
bugbui <b.chayuda@gmail.com>
2019-02-24 13:37:47 +0700

    bui atm change

--- atm.php
+++ atm.php
@@ -18,7 +18,10 @@
     
 <?php
 
-if (!empty($_POST['withdraw'])) {
+if ( !empty($_POST['withdraw']) &&
+($_POST['withdraw']%10)==0 &&
+($_POST['withdraw']%100)!=10   &&
+($_POST['withdraw']%100)!=30  ) {
     
     
     
@@ -47,28 +50,13 @@ if (!empty($_POST['withdraw'])) {
             $a=$a-20;
         }
 
-    $l = 0; 
-    
-    if (($a%20) <> 0){
-
-        for ($l=0;$a>=50 ;$l++){
-            $a=$a-50;
-        }
+    }else{
     
         for ($m=0;$a>=20 ;$m++){
-            $a=$a-20;
+        $a=$a-20;
         }
-
-    }else {
-
-        for ($m=0;$a>=20 ;$m++){
-            $a=$a-20;
-        }
-        
     }
-
     
-if ($a == 0) {
 
 echo "<br><br><h1>Requested Amount : </h1>".$_POST['withdraw']. "Baht";
 echo "<br><br><h1>You will get : </h1>";
@@ -77,9 +65,6 @@ echo "<br>".$j." five-hundred-baht note";
 echo "<br>".$k." hundred-baht note";
 echo "<br>".$l." fifty-baht note";
 echo "<br>".$m." twenty-baht note";
-
-}
-
 }
 
 
